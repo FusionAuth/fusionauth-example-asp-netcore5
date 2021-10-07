08a913965f2f461a72c08d0a8975a11df61ed349
Dan Moore <dan@fusionauth.io>
2021-10-07 09:07:15 -0600

    Pulling over changes needed for  claim check.

--- SampleApp/Startup.cs
+++ SampleApp/Startup.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Threading.Tasks;
+using System.Text.Json;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +11,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Logging;
 using Microsoft.IdentityModel.Tokens;
 
@@ -17,6 +19,7 @@ namespace SampleApp
 {
     public class Startup
     {
+ 
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -27,12 +30,17 @@ namespace SampleApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole();                
+            });
+
+            ILogger logger = loggerFactory.CreateLogger<Startup>();
             JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
 
             // Configure your policies
             services.AddAuthorization(options =>
-                options.AddPolicy("Registered",
-                policy => policy.RequireClaim("applicationId", Configuration["SampleApp:ClientId"])));
+                options.AddPolicy("Registered", policy => policy.RequireClaim("aud", Configuration["SampleApp:ClientId"])));
             services.AddRazorPages();
 
             services.AddAuthentication(options =>
@@ -46,12 +54,22 @@ namespace SampleApp
                 })
                 .AddOpenIdConnect("oidc", options =>
                 {
+
                     options.Authority = Configuration["SampleApp:Authority"];
                     options.ClientId = Configuration["SampleApp:ClientId"];
                     options.ClientSecret = Configuration["SampleApp:ClientSecret"];
+                    options.Scope.Add("openid");
+
+                    // leave this in, otherwise the aud claim is removed. See https://stackoverflow.com/questions/69289426/missing-aud-claim-in-asp-net-core-policy-check-prevents-authorization-but-it for more
+                    options.ClaimActions.Remove("aud");
 
                     options.ResponseType = "code";
                     options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = true,
+                        ValidAudience = Configuration["SampleApp:ClientId"]
+                    };
                 });
 
         }
